1f0bb64da62831fa9ff4a3b5186a58ae9e57efcd
Demjke <36662752+Demjke@users.noreply.github.com>
2021-02-18 12:41:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+  $('.slider').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="img/prev-arrow.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="img/next-arrow.svg" alt=""></button>',
+  });
+});
